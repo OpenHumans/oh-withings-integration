a3f1db331a3eb66246c503ace1979421fb63b244
Stephanie Unna <steph.unna@gmail.com>
2018-03-02 11:16:56 +0000

    Adding project root

--- demotemplate/settings.py
+++ demotemplate/settings.py
@@ -14,6 +14,7 @@ import os
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
