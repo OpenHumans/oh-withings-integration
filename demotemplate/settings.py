1741f0f7f6fd5f35e5067323d46cd4b5ebf947e4
Stephanie Unna <steph.unna@gmail.com>
2018-03-02 13:23:20 +0000

    Adding context processor as in data uploader

--- demotemplate/settings.py
+++ demotemplate/settings.py
@@ -82,6 +82,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'main.context_processors.read_config'
             ],
         },
     },
