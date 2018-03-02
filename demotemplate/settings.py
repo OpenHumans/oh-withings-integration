27002cac20b19dc58356544fc35df8b2c9d78a26
Stephanie Unna <steph.unna@gmail.com>
2018-03-02 11:18:24 +0000

    Setting if statement for auto decide whether DEBUG true

--- demotemplate/settings.py
+++ demotemplate/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False if os.getenv('DEBUG', '').lower() == 'false' else True
 
 ALLOWED_HOSTS = []
 
