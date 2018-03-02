5cfbcd235be45f0bac769202c615b27154947705
Stephanie Unna <steph.unna@gmail.com>
2018-03-02 13:06:53 +0000

    Deleting irrelevant comments

--- demotemplate/settings.py
+++ demotemplate/settings.py
@@ -17,10 +17,6 @@ import dj_database_url
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
