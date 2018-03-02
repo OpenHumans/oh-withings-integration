c348e9bcf8af3029d23f682229cf84433fe0c057
Stephanie Unna <steph.unna@gmail.com>
2018-03-02 13:22:54 +0000

    Matching whitenoise config to data uploader

--- demotemplate/settings.py
+++ demotemplate/settings.py
@@ -66,13 +66,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
-
-MIDDLEWARE_CLASSES = (
-    # Simplified static file serving.
-    # https://warehouse.python.org/project/whitenoise/
-    'whitenoise.middleware.WhiteNoiseMiddleware'
-)
 
 ROOT_URLCONF = 'demotemplate.urls'
 
