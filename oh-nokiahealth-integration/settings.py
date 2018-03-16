064efbc6fa283a8dce1a4e2ac1f10d6057a2ce4e
Stephanie Unna <steph.unna@gmail.com>
2018-03-16 14:11:44 +0000

    Removing include time

--- oh-nokiahealth-integration/settings.py
+++ oh-nokiahealth-integration/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import dj_database_url
-import time
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
