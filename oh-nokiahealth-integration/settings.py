3404f1c053967079494ac6d28e76b461602a0648
Stephanie Unna <steph.unna@gmail.com>
2018-04-04 11:39:03 +0100

    Adding sentry app in settings

--- oh-nokiahealth-integration/settings.py
+++ oh-nokiahealth-integration/settings.py
@@ -66,6 +66,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'raven.contrib.django.raven_compat'
 
     'datauploader.apps.DatauploaderConfig',
     'open_humans.apps.OpenHumansConfig',
