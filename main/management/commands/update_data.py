13b9bcea20fdad922f78c46104953812cbac2616
Stephanie Unna <steph.unna@gmail.com>
2018-04-11 11:40:45 +0100

    Trying out schedule

--- main/management/commands/update_data.py
+++ main/management/commands/update_data.py
@@ -1,6 +1,8 @@
 from django.core.management.base import BaseCommand
 from main.models import NokiaHealthMember
 from main.views import fetch_nokia_data
+import schedule
+import time
 
 
 class Command(BaseCommand):
@@ -10,3 +12,10 @@ class Command(BaseCommand):
         users = NokiaHealthMember.objects.all()
         for user in users:
             fetch_nokia_data(user, user.access_token)
+
+    schedule.every().wednesday.at("20:00").do(handle)
+
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
