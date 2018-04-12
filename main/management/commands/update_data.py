efba26239e18a2215f3e51e6ddb86ed03803608f
Stephanie Unna <steph.unna@gmail.com>
2018-04-12 17:23:11 +0100

    Making print every minute to test scheduler

--- main/management/commands/update_data.py
+++ main/management/commands/update_data.py
@@ -9,11 +9,13 @@ class Command(BaseCommand):
     help = 'Updates data for all members'
 
     def handle(self, *args, **options):
-        users = NokiaHealthMember.objects.all()
-        for user in users:
-            fetch_nokia_data(user, user.access_token)
+        # users = NokiaHealthMember.objects.all()
+        # for user in users:
+        #     fetch_nokia_data(user, user.access_token)
+        print("hello")
 
-    schedule.every().wednesday.at("20:00").do(handle)
+    # schedule.every().wednesday.at("20:00").do(handle)
+    schedule.every(1).minutes.do(handle)
 
 
 while True:
