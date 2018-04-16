64c9bfc4f2c512320932374271d2b888879cf525
Stephanie Unna <steph.unna@gmail.com>
2018-04-16 10:41:14 +0100

    Setting up logging in management tasks

--- main/management/commands/update_data.py
+++ main/management/commands/update_data.py
@@ -1,8 +1,11 @@
 from django.core.management.base import BaseCommand
 from main.models import NokiaHealthMember
 from main.views import fetch_nokia_data
+import logging
 import schedule
 import time
+
+logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
@@ -12,7 +15,7 @@ class Command(BaseCommand):
         # users = NokiaHealthMember.objects.all()
         # for user in users:
         #     fetch_nokia_data(user, user.access_token)
-        print("hello")
+        logger.debug('hello')
 
     # schedule.every().wednesday.at("20:00").do(handle)
     schedule.every(1).minutes.do(handle)
