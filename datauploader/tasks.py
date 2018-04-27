666fc87dcc29ed11a3a63dbb0ac979e4abc89959
Stephanie Unna <steph.unna@gmail.com>
2018-04-27 13:39:35 +0100

    Move data fetch to celery task out of views

--- datauploader/tasks.py
+++ datauploader/tasks.py
@@ -23,6 +23,77 @@ logger = logging.getLogger(__name__)
 
 
 @shared_task
+def fetch_nokia_data(userid, queryoauth, nokia_data):
+    '''
+    Fetch all nokia health data for a given user
+    '''
+    nokia_urls = [
+        {'name': 'activity',
+         'url': 'https://api.health.nokia.com/v2/measure?action=getactivity',
+         'period': ''},
+        {'name': 'measure',
+         'url': 'https://api.health.nokia.com' +
+                '/measure?action=getmeas&userid=' + str(userid),
+         'period': ''},
+        {'name': 'intraday',
+         'url': 'https://api.health.nokia.com' +
+                '/v2/measure?action=getintradayactivity',
+         'period': ''},
+        {'name': 'sleep',
+         'url': 'https://api.health.nokia.com/v2/sleep?' +
+                'action=get&startdate=1387234800&enddate=1387258800' +
+                str(userid),
+         'period': ''},
+        {'name': 'sleep_summary',
+         'url': 'https://api.health.nokia.com' +
+                '/v2/sleep?action=getsummary',
+         'period': ''},
+        {'name': 'workouts',
+         'url': 'https://api.health.nokia.com' +
+                '/v2/measure?action=getworkouts',
+         'period': ''}
+    ]
+
+    dataarray = []
+    for url in nokia_urls:
+        thisfetch = rr.get(url=url['url'], auth=queryoauth, realms=["Nokia"])
+        dataarray.append(thisfetch.text)
+
+    datastring = combine_nh_data(dataarray)
+    print(datastring)
+    return datastring
+
+
+def combine_nh_data(dataarray):
+    """
+    Combine Nokia Health data for all endpoints (activity, measure, intraday,
+    sleep, sleep summary, workouts) into a single string.
+    """
+    endpoints = ['"activity":', ',"measure":', ',"intraday":',
+                 ',"sleep":', ',"sleepsummary":', ',"workouts":']
+
+    datastring = '{'
+    for i in range(0, len(endpoints)-1):
+        datastring += endpoints[i] + dataarray[i]
+
+    datastring += '}'
+    return datastring
+
+
+def get_existing_nokia(oh_access_token):
+    member = api.exchange_oauth2_member(oh_access_token)
+    for dfile in member['data']:
+        if 'nokiahealth' in dfile['metadata']['tags']:
+            # get file here and read the json into memory
+            tf_in = tempfile.NamedTemporaryFile(suffix='.json')
+            tf_in.write(requests.get(dfile['download_url']).content)
+            tf_in.flush()
+            nokia_data = json.load(open(tf_in.name))
+            return nokia_data
+    return []
+
+
+@shared_task
 def xfer_to_open_humans(user_data, metadata, oh_id, num_submit=0, **kwargs):
     """
     Transfer data to Open Humans.
