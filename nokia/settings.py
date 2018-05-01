2a7ef9325602d3a9fde27662783ebf1ceadfc11c
Stephanie Unna <steph.unna@gmail.com>
2018-05-01 11:22:55 +0100

    Fixing remote redirect bug

--- nokia/settings.py
+++ nokia/settings.py
@@ -76,11 +76,12 @@ if REMOTE is True:
 rr = RespectfulRequester()
 rr.register_realm("Nokia", max_requests=3600, timespan=3600)
 
-if REMOTE is False:
-    NOKIA_CALLBACK_URL = 'http://127.0.0.1:5000/complete_nokia'
+if REMOTE:
+    print("remote print")
+    NOKIA_CALLBACK_URL = 'http://oh-nokiahealth-integration.herokuapp.com/complete_nokia'
 else:
-    NOKIA_CALLBACK_URL =\
-        'http://oh-nokiahealth-integration.herokuapp.com/complete_nokia'
+    print("local print")
+    NOKIA_CALLBACK_URL = 'http://127.0.0.1:5000/complete_nokia'
 
 # Application definition
 
