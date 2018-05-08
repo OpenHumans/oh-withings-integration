dda3f2bdc746e5c4cc17faa05b782e37aa259191
Stephanie Unna <steph.unna@gmail.com>
2018-05-08 10:12:20 +0100

    removing data fetch call and messages in templates

--- main/views.py
+++ main/views.py
@@ -49,7 +49,7 @@ def complete_nokia(request):
 
     if nokia_member:
         # Fetch user's data from Nokia (update the data if it already existed)
-        process_nokia(oh_id)
+        # process_nokia(oh_id)
         context = {'tokeninfo': 'Fetching data...',
                    'oh_proj_page': settings.OH_ACTIVITY_PAGE}
         return render(request, 'main/complete_nokia.html', context=context)
