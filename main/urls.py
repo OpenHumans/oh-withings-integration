5b32c4f87b5b98f8d071694a82c887aa928f852e
Stephanie Unna <steph.unna@gmail.com>
2018-05-15 21:38:26 +0100

    Adding url paths for new templates

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('complete', views.complete, name='complete'),
-    path('complete_nokia', views.complete_nokia, name='complete_nokia'),
+    path('complete/', views.complete, name='complete'),
+    path('complete_nokia/', views.complete_nokia, name='complete_nokia'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('update_data/', views.update_data, name='update_data'),
+    path('remove_nokia/', views.remove_nokia, name='remove_nokia')
 ]
