70ef4cae26262a8555c5fa9374a3923653c507c3
Mike Escalante <mcescalante@gmail.com>
2018-11-29 16:54:19 -0500

    move post params to body for oauth migration

--- main/management/commands/migrate_oauth.py
+++ main/management/commands/migrate_oauth.py
@@ -8,19 +8,27 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         nokia_users = NokiaHealthMember.objects.all()
-        for user in nokia_users:
+        for user in nokia_users[:5]:
             if user.oauth_token:
                 print("user {} has a token".format(user.userid))
                 baseUrl = 'https://account.withings.com/oauth2/token'
                 # Construct custom token refresh URL
                 # concatenates oauth token + secret and pass it as refresh_token
-                migrateUrl = baseUrl + \
-                            "?grant_type=refresh_token" + \
-                            "&client_id=" + settings.NOKIA_CLIENT_ID + \
-                            "&client_secret=" + settings.NOKIA_CLIENT_SECRET + \
-                            "&refresh_token=" + user.oauth_token + ":" + user.oauth_token_secret
-                print("Token migration url: " + migrateUrl)  
-                r = requests.post(migrateUrl)
+                payload = {
+                    "grant_type": "refresh_token",
+                    "client_id": str(settings.NOKIA_CLIENT_ID),
+                    "client_secret": str(settings.NOKIA_CLIENT_SECRET),
+                    "refresh_token": str(user.oauth_token) + ":" + str(user.oauth_token_secret)
+                }
+                # migrateUrl = baseUrl + \
+                #             "?grant_type=refresh_token" + \
+                #             "&client_id=" + settings.NOKIA_CLIENT_ID + \
+                #             "&client_secret=" + settings.NOKIA_CLIENT_SECRET + \
+                #             "&refresh_token=" + user.oauth_token + ":" + user.oauth_token_secret
+                # print("Token migration url: " + migrateUrl)  
+                print("Token payload")
+                print(payload)
+                r = requests.post(baseUrl, data = payload)
                 q = r.json()
                 print(q)
                 # Update user data & save
