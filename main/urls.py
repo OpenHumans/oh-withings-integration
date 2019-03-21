9b1539c564a2a3d47f7b1f211f1814c13202e701
Mike Escalante <mcescalante@gmail.com>
2019-03-21 12:49:40 -0400

    update templates, bump django version

--- main/urls.py
+++ main/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('complete_nokia/', views.complete_nokia, name='complete_nokia'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('update_data/', views.update_data, name='update_data'),
-    path('remove_nokia/', views.remove_nokia, name='remove_nokia')
+    path('remove_nokia/', views.remove_nokia, name='remove_nokia'),
+    path('about/', views.about, name='about'),
 ]
