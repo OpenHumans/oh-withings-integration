03c546b2bb859d86e877543645be5737e6f7104f
Mike Escalante <mcescalante@gmail.com>
2019-10-18 10:50:51 -0400

    update templates (#32)
    * update templates, bump django version
    * update meta description
    * add withings logo

--- datauploader/apps.py
+++ datauploader/apps.py
@@ -1,5 +1,4 @@
 from django.apps import AppConfig
 
-
 class DatauploaderConfig(AppConfig):
     name = 'datauploader'
--- main/views.py
+++ main/views.py
@@ -36,6 +36,16 @@ def index(request):
         return render(request, 'main/index.html', context=context)
 
 
+def about(request):
+    """
+    Share further details about the project.
+    """
+    context = {'client_id': settings.OPENHUMANS_CLIENT_ID,
+            #    'redirect_uri': '{}/complete'.format(settings.OPENHUMANS_APP_BASE_URL),
+               'oh_proj_page': settings.OH_ACTIVITY_PAGE}
+    return render(request, 'main/about.html', context=context)
+    
+
 def complete(request):
     """
     Receive user from Open Humans and store.
